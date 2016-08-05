ed48cf9f09a4697177cc7b5999c20dcb98c8f738
MarkLHF <2751867750@qq.com>
2016-08-05 23:06:01 +0800

    这次是把之前没有填完的坑填完了，把改进版的感知器算法做出来了，其实的确很简单，但是当时就是没有理解或许是理论上的理解不够导致的吧。
    Signed-off-by: MarkLHF <2751867750@qq.com>

--- Example/Example3_2.m
+++ Example/Example3_2.m
@@ -2,13 +2,13 @@ clc;
 clear;
 close all;
 %这个是练习3.1的实例程序
-%------ 这个算法是感知器算法，以下为一些重要的说明：
-%       1.这个算法只能用在可以线性可分的分类问题中
-%       2.这个算法会无限次循环下去，直到找到一个可以完全分类的权向量
+%------ 这个算法是改进的感知器算法，以下为一些重要的说明：
+%       1.这个算法是基于感知器算法的，所以大部分都是感知器算法的内容
+%       2.现在的改进部分是加入了奖励和惩罚机制，而不再是单纯的判断了。
 randn('seed',0);
 P1=[1  1];
-m1=[1 14]';
-m2=[14 1]';
+m1=[1 25]';
+m2=[8 1]';
 sita=sqrt(4);
 S1=(sita^2)*eye(2);
 S2=S1;
@@ -26,10 +26,12 @@ X1xlf=[x1(1:(N/2),:);x2(1:(N/2),:)];
 plot(x1_Source(((N/2)+1:N),1),x1_Source(((N/2)+1:N),2),'mo','MarkerSize',3);hold on;
 plot(x2_Source(((N/2)+1:N),1),x2_Source(((N/2)+1:N),2),'mo','MarkerSize',3);hold on;
 X1xln=[x1((N/2)+1:N,:);x2((N/2)+1:N,:)];
+title('原始数据');
 %在第二个图中显示
 figure(2);
 plot(x1_Source((1:N/2),1),x1_Source((1:N/2),2),'ro','MarkerSize',3);hold on;
 plot(x2_Source((1:N/2),1),x2_Source((1:N/2),2),'bo','MarkerSize',3);hold on;
+title('分类后的数据');
 %---------------------------------------------------------------------------------------------------------
 x0=1/N*sum(X1xlf);
 plot(x0(1),x0(2),'g+','MarkerSize',7);hold on;
@@ -51,21 +53,24 @@ plot(n1,n2,'g.','MarkerSize',3);hold on;
 %权向量的形式为：ax1+bx2+c=0<---->w=[a,b,c]'
 %计算系数siertax
 ww1=[x1,ones(size(x1,1),1)]; 
-ww2=[x2,ones(size(x2,1),1)]; 
-X=[ww1;-ww2];
+ww2=[x2,-ones(size(x2,1),1)]; 
+X=[ww1;ww2];
 W=w0';
 %W=ones(size(X,2),1);
 ok=0;
 %初始化基本参数
 Ro=0.05;
-Rot=Ro;
 k=1;%这个数是用来调试的
 %循环体
 while(ok==0)
     for n=1:size(X,1)
-        if (W'*X(n,:)'<0) 
+        if (W'*X(n,:)'<=0 && X(n,3)==1) 
             k=k+1;
             W=W+Ro*X(n,:)';
+            break;
+        elseif (W'*X(n,:)'>=0 && X(n,3)==-1) 
+            k=k+1;
+            W=W-Ro*X(n,:)';
             break;
         else
             if (n==size(X,1)) 
